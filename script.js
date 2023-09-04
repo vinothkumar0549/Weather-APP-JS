75b23881f7f5c2c48a5f2c96fd56a328630ed095
Vinoth <vinothkumar0549@gmail.com>
2023-09-04 14:08:33 +0530

    Updated

--- script.js
+++ script.js
@@ -32,6 +32,10 @@ async function result() {
         data.datetime +
         " Description: " +
         data.weather.description;
+      let days = data.weather.icon;
+      const img = document.createElement("img");
+      img.src = `./icons/${days}.png`;
+      div.appendChild(img);
       document.getElementById("maindiv").appendChild(div);
     }
   } catch (err) {
@@ -44,32 +48,39 @@ function currentlocation() {
   let lat = document.createElement("p");
   let lon = document.createElement("p");
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      lat.innerHTML = "Latitude: " + position.coords.latitude;
-      lon.innerHTML = "Longitude: " + position.coords.longitude;
-      document.getElementById("maindiv").appendChild(lat);
-      document.getElementById("maindiv").appendChild(lon);
-      fetch(
-        `https://api.weatherbit.io/v2.0/forecast/daily?lat=${position.coords.latitude}&lon=${position.coords.longitude}&days=7&key=34d92fae5c9a4c34905b68ee125a90cc`
-      )
-        .then((response) => response.json())
-        .then((response) => {
-          const div = document.createElement("p");
-          div.innerHTML = "City: " + response.city_name;
-          document.getElementById("maindiv").appendChild(div);
-          for (let data of response.data) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        lat.innerHTML = "Latitude: " + position.coords.latitude;
+        lon.innerHTML = "Longitude: " + position.coords.longitude;
+        document.getElementById("maindiv").appendChild(lat);
+        document.getElementById("maindiv").appendChild(lon);
+        fetch(
+          `https://api.weatherbit.io/v2.0/forecast/daily?lat=${position.coords.latitude}&lon=${position.coords.longitude}&days=7&key=34d92fae5c9a4c34905b68ee125a90cc`
+        )
+          .then((response) => response.json())
+          .then((response) => {
             const div = document.createElement("p");
-            div.innerHTML =
-              "Temp: " +
-              data.temp +
-              "  Date: " +
-              data.datetime +
-              "  Description: " +
-              data.weather.description;
+            div.innerHTML = "City: " + response.city_name;
             document.getElementById("maindiv").appendChild(div);
-          }
-        });
-    });
+            for (let data of response.data) {
+              const div = document.createElement("p");
+              div.innerHTML =
+                "Temp: " +
+                data.temp +
+                "  Date: " +
+                data.datetime +
+                "  Description: " +
+                data.weather.description;
+              let days = data.weather.icon;
+              const img = document.createElement("img");
+              img.src = `./icons/${days}.png`;
+              div.appendChild(img);
+              document.getElementById("maindiv").appendChild(div);
+            }
+          });
+      },
+      (err) => alert("Not Access to the Location")
+    );
   } else {
     alert("Sorry for the Inconvienece");
   }
